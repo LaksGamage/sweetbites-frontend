606380c8ee6cdab26323c85d2a06895731b19546
Lakshan Gamage <your-github-email@example.com>
2026-05-05 11:22:17 +0530

    Day2 : Improved navbar functionalities

---
+++ src/assets/asset.js
@@ -0,0 +1,11 @@
+import profile_icon from "./navbar-user.png";
+import nav_cart_icon from "./shopping-cart.png";
+import search_icon from "./search.png";
+
+
+
+export const assets = {
+   profile_icon,
+   nav_cart_icon, 
+   search_icon,
+};
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
-    const navigate = useNavigate();
-    const [user, setUser] = useState(null)
-    const [isSeller, setIsSeller] = useState(false)
 
-    const value ={navigate, user, setUser, setIsSeller, isSeller}
+    const navigate = useNavigate();
+    const [user, setUser] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(false)
+
+    const value ={navigate, user, setUser, setIsSeller, isSeller,
+        showUserLogin, setShowUserLogin
+    }
 
     return <AppContext.Provider value={value}>
         {children}
